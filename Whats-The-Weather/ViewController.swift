ec20c66588f2cf3804f90767f81895f781f2c923
akilkumar28 <akil.psn@gmail.com>
2017-06-04 21:09:00 +0530

    Initial Commit

---
+++ Whats-The-Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Whats-The-Weather
+//
+//  Created by AKIL KUMAR THOTA on 6/4/17.
+//  Copyright © 2017 Akil Kumar Thota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
