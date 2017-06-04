738ceee145f23d3314750e9c4563de0b8b2d9ea5
akilkumar28 <akil.psn@gmail.com>
2017-06-04 23:43:29 +0530

    Initial Commit
    added the project and made an initial commit

--- Whats-The-Weather/ViewController.swift
+++ Whats-The-Weather/ViewController.swift
@@ -8,18 +8,100 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var txtFld: UITextField!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func submitPrssd(_ sender: Any) {
+        
+        if !((txtFld.text?.isEmpty)!) {
+            
+            if let name = txtFld.text?.replacingOccurrences(of: " ", with: "-") {
+                
+                
+                if let url = URL(string: "http://www.weather-forecast.com/locations/"+name+"/forecasts/latest") {
+                    
+                    let request = URLRequest(url: url)
+                    
+                    var message = ""
+                    
+                    let task = URLSession.shared.dataTask(with: request) { (Data, URLResponse, Error) in
+                        
+                        if Error != nil {
+                            print("Error Occured")
+                        } else {
+                            
+                            
+                            if let unrappedData = Data {
+                                
+                                let answer = NSString(data: unrappedData, encoding: String.Encoding.utf8.rawValue)
+                                var stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                                
+                                if let contentArray = answer?.components(separatedBy: stringSeparator) {
+                                    
+                                    
+                                    if contentArray.count > 1 {
+                                        
+                                        stringSeparator = "</span>"
+                                        
+                                        let newArray = contentArray[1].components(separatedBy: stringSeparator)
+                                        
+                                        if newArray.count > 1 {
+                                            
+                                            
+                                            message = newArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                            
+                                            
+                                        }
+                                    }
+                                }
+                                
+                                
+                            }
+                        }
+                        if message == "" {
+                            message = "The weather there couldn't be found. Please try again."
+                        }
+                        
+                        DispatchQueue.main.async {
+                            
+                            self.resultsLabel.text = message
+                        }
+                        
+                    }
+                    task.resume()
+                    
+                    
+                    
+                } else {
+                    resultsLabel.text = "The weather there couldn't be found. Please try again."
+                }
+            } else {
+                resultsLabel.text = "Please enter the name of the city properly."
+            }
+        } else {
+            resultsLabel.text = "Please Enter Some name of the city."
+        }
     }
-
-
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
